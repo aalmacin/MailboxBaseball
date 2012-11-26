d938f65e011fbb74d68a9a4fd99ac8c6bb1d04f0
Developer <Developer@192.168.0.20>
2012-11-26 12:26:43 -0500

    First commit

---
+++ MailboxBaseball/src/com/aldrinmike/mailboxbaseball/MainMenuActivity.java
@@ -0,0 +1,141 @@
+package com.aldrinmike.mailboxbaseball;
+
+import javax.microedition.khronos.opengles.GL10;
+
+import org.anddev.andengine.engine.Engine;
+import org.anddev.andengine.engine.camera.Camera;
+import org.anddev.andengine.engine.options.EngineOptions;
+import org.anddev.andengine.engine.options.EngineOptions.ScreenOrientation;
+import org.anddev.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
+import org.anddev.andengine.entity.scene.Scene;
+import org.anddev.andengine.entity.scene.menu.MenuScene;
+import org.anddev.andengine.entity.scene.menu.MenuScene.IOnMenuItemClickListener;
+import org.anddev.andengine.entity.scene.menu.item.IMenuItem;
+import org.anddev.andengine.entity.scene.menu.item.TextMenuItem;
+import org.anddev.andengine.entity.scene.menu.item.decorator.ColorMenuItemDecorator;
+import org.anddev.andengine.entity.sprite.Sprite;
+import org.anddev.andengine.entity.util.FPSLogger;
+import org.anddev.andengine.opengl.font.Font;
+import org.anddev.andengine.opengl.font.FontFactory;
+import org.anddev.andengine.opengl.texture.Texture;
+import org.anddev.andengine.opengl.texture.TextureOptions;
+import org.anddev.andengine.opengl.texture.region.TextureRegion;
+import org.anddev.andengine.opengl.texture.region.TextureRegionFactory;
+import org.anddev.andengine.ui.activity.BaseGameActivity;
+
+import android.graphics.Color;
+
+public class MainMenuActivity extends BaseGameActivity  implements IOnMenuItemClickListener {
+
+
+	private final static int CAMERA_WIDTH = 480;
+	private final static int CAMERA_HEIGHT = 800;
+	
+	private final static int MENU_START = 0;
+	private final static int MENU_HELP = 1;
+	private final static int MENU_HIGHSCORES = 2;
+	private final static int MENU_EXIT = 3;
+	
+	private Camera mCamera;
+	private Font mFont;
+	private Texture mFontTexture;
+	private Texture mBackgroundTexture;
+	private TextureRegion mBackgroundTextureRegion;
+	private Scene mMainScene;
+	private MenuScene mStaticMenuScene;
+	
+
+
+	
+	@Override
+	public Engine onLoadEngine() {
+		this.mCamera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
+		return new Engine(new EngineOptions(true, ScreenOrientation.PORTRAIT, 
+				new RatioResolutionPolicy(CAMERA_WIDTH,CAMERA_HEIGHT), this.mCamera));
+	}
+
+	@Override
+	public void onLoadResources() {
+		mFontTexture = new Texture(256, 256,TextureOptions.BILINEAR_PREMULTIPLYALPHA);
+		FontFactory.setAssetBasePath("font/");
+		this.mFont = FontFactory.createFromAsset(this.mFontTexture, this, "kulminoituva.ttf", 32, true, Color.BLUE);
+		this.mEngine.getTextureManager().loadTexture(this.mFontTexture);
+		this.mEngine.getFontManager().loadFont(this.mFont);
+		
+		
+		this.mBackgroundTexture = new Texture(1024,1024,TextureOptions.BILINEAR_PREMULTIPLYALPHA);
+		this.mBackgroundTextureRegion = TextureRegionFactory.createFromAsset(mBackgroundTexture, this, "gfx/background.png", 0, 0);
+		this.mEngine.getTextureManager().loadTexture(this.mBackgroundTexture);
+	}
+
+	@Override
+	public Scene onLoadScene() {
+		this.mEngine.registerUpdateHandler(new FPSLogger());
+		this.createStaticMenuScene();
+		this.mMainScene = new Scene(1);
+		final int centerX = (CAMERA_WIDTH-this.mBackgroundTextureRegion.getWidth());
+		final int centerY = (CAMERA_HEIGHT-this.mBackgroundTextureRegion.getHeight());
+//		final int centerX = (this.mBackgroundTextureRegion.getWidth()/2);
+//		final int centerY = (this.mBackgroundTextureRegion.getHeight()/2);
+		
+		final Sprite main = new Sprite(centerX,centerY,this.mBackgroundTextureRegion);
+		mMainScene.getLastChild().attachChild(main);
+		mMainScene.setChildScene(mStaticMenuScene);
+		return this.mMainScene;
+	}
+
+	private void createStaticMenuScene() {		
+		this.mStaticMenuScene = new MenuScene(this.mCamera);
+		final IMenuItem startMenuItem = new ColorMenuItemDecorator(
+				new TextMenuItem(MENU_START, mFont, "Start"), 0.5f, 0.5f, 0.5f, 1.0f, 1.0f, 1.0f);
+		startMenuItem.setBlendFunction(GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA);
+		this.mStaticMenuScene.addMenuItem(startMenuItem);
+		
+		final IMenuItem helpMenuItem = new ColorMenuItemDecorator(
+				new TextMenuItem(MENU_HELP, mFont, "Help"), 0.5f, 0.5f, 0.5f, 1.0f, 1.0f, 1.0f);
+		helpMenuItem.setBlendFunction(GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA);
+		this.mStaticMenuScene.addMenuItem(helpMenuItem);
+		
+		final IMenuItem highScoreMenuItem = new ColorMenuItemDecorator(
+				new TextMenuItem(MENU_HIGHSCORES, mFont, "High Scores"), 0.5f, 0.5f, 0.5f, 1.0f, 1.0f, 1.0f);
+		highScoreMenuItem.setBlendFunction(GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA);
+		this.mStaticMenuScene.addMenuItem(highScoreMenuItem);
+		
+		final IMenuItem exitMenuItem = new ColorMenuItemDecorator(
+				new TextMenuItem(MENU_EXIT, mFont, "Exit"), 0.5f, 0.5f, 0.5f, 1.0f, 1.0f, 1.0f);
+		exitMenuItem.setBlendFunction(GL10.GL_SRC_ALPHA, GL10.GL_ONE_MINUS_SRC_ALPHA);
+		this.mStaticMenuScene.addMenuItem(exitMenuItem);		
+		
+		this.mStaticMenuScene.buildAnimations();
+		this.mStaticMenuScene.setBackgroundEnabled(false);
+		this.mStaticMenuScene.setOnMenuItemClickListener(this);
+	}
+
+	@Override
+	public void onLoadComplete() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public boolean onMenuItemClicked(MenuScene pMenuScene, IMenuItem pMenuItem,
+			float pMenuItemLocalX, float pMenuItemLocalY) {
+		switch (pMenuItem.getID()) {
+		case MENU_START:
+			
+//			Toast.makeText(MainMenuActivity.this,"Start Selected", Toast.LENGTH_SHORT).show();
+			return true;
+		case MENU_HELP:
+//			Toast.makeText(MainMenuActivity.this,"Help Selected", Toast.LENGTH_SHORT).show();
+			return true;
+		case MENU_HIGHSCORES:
+//			Toast.makeText(MainMenuActivity.this,"High Score Selected", Toast.LENGTH_SHORT).show();
+			return true;
+		case MENU_EXIT:
+			this.finish();
+			return true;
+		default:
+			return false;
+		}
+	}
+}
---
+++ MailboxBaseball/src/com/aldrinmike/mailboxbaseball/SplashActivity.java
@@ -0,0 +1,73 @@
+package com.aldrinmike.mailboxbaseball;
+
+
+import org.anddev.andengine.engine.Engine;
+import org.anddev.andengine.engine.camera.Camera;
+import org.anddev.andengine.engine.options.EngineOptions;
+import org.anddev.andengine.engine.options.EngineOptions.ScreenOrientation;
+import org.anddev.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
+import org.anddev.andengine.entity.scene.Scene;
+import org.anddev.andengine.entity.sprite.Sprite;
+import org.anddev.andengine.entity.util.FPSLogger;
+import org.anddev.andengine.opengl.texture.Texture;
+import org.anddev.andengine.opengl.texture.TextureOptions;
+import org.anddev.andengine.opengl.texture.region.TextureRegion;
+import org.anddev.andengine.opengl.texture.region.TextureRegionFactory;
+import org.anddev.andengine.ui.activity.BaseGameActivity;
+
+import android.content.Intent;
+import android.os.Handler;
+
+public class SplashActivity extends BaseGameActivity {
+
+	private final static int CAMERA_WIDTH = 480;
+	private final static int CAMERA_HEIGHT = 800;
+//	private int CAMERA_WIDTH;
+//	private int CAMERA_HEIGHT;
+	private Camera mCamera;
+	private Texture mTexture;
+	private TextureRegion mSplashTextureRegion;
+	private Handler mHandler;
+	
+	public Engine onLoadEngine() {
+//		Point pointSize = new Point();
+//		getWindowManager().getDefaultDisplay().getSize(pointSize);
+//		int CAMERA_WIDTH = pointSize.x;
+//		int CAMERA_HEIGHT = pointSize.y;
+		mHandler = new Handler();
+		this.mCamera = new Camera(0,0,CAMERA_WIDTH,CAMERA_HEIGHT);
+		return new Engine(new EngineOptions(true, ScreenOrientation.PORTRAIT, 
+				new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT)
+				, this.mCamera));
+	}
+
+	public void onLoadResources() {
+		this.mTexture = new Texture(1024,1024,TextureOptions.BILINEAR_PREMULTIPLYALPHA);
+		this.mSplashTextureRegion = TextureRegionFactory.createFromAsset(this.mTexture, this, "gfx/splashScreen.png", 0, 0);
+		this.mEngine.getTextureManager().loadTexture(this.mTexture);
+	}
+
+	public Scene onLoadScene() {
+		this.mEngine.registerUpdateHandler(new FPSLogger());
+		final Scene scene = new Scene(1);
+		final int centerX = (CAMERA_WIDTH-this.mSplashTextureRegion.getWidth());
+		final int centerY = (CAMERA_HEIGHT-this.mSplashTextureRegion.getHeight());
+		
+		final Sprite splash = new Sprite(centerX,centerY,this.mSplashTextureRegion);
+		scene.getLastChild().attachChild(splash);
+		return scene;
+	}
+
+	public void onLoadComplete() {
+		mHandler.postDelayed(mLaunchTask,3000);
+	}
+	
+	private Runnable mLaunchTask = new Runnable() {
+		
+		@Override
+		public void run() {
+			Intent myIntent = new Intent(SplashActivity.this,MainMenuActivity.class);
+			SplashActivity.this.startActivity(myIntent);
+		}
+	};
+}
