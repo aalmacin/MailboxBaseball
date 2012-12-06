ba3a5db8af0c0e67ec9e634403a55a1c7b8be28e
mike <mfjy@outlook.com>
2012-12-06 15:57:05 -0500

    DB limit query changed

--- MailboxBaseball/src/com/aldrinmike/mailboxbaseball/Controller.java
+++ MailboxBaseball/src/com/aldrinmike/mailboxbaseball/Controller.java
@@ -106,7 +106,7 @@ public class Controller {
 		// Open the database and take the values and quantity into a tempCursor.
 		DBAdapter dbHelper = new DBAdapter();
 		Cursor tempCursor = dbHelper.openToRead().query(HIGH_SCORE_TABLE, new String[] { PLAYER_ID, PLAYER_SCORE }, 
-				null, null, null, null, PLAYER_SCORE, "10");
+				null, null, null, null, PLAYER_SCORE+" DESC", "10");
 		boolean belongToTop10 = false;
 		if (tempCursor.moveToFirst())
 			// Go through each item in the cursor and add the values into the
